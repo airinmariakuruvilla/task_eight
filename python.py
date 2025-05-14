013fc069d0aa74c80d00b242087cd1cb5a6b80ea
airinmariakuruvilla <85423470+airinmariakuruvilla@users.noreply.github.com>
2025-05-14 12:24:37 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-print("airin maria kuruvilla")
+print("hello!! this is airin maria kuruvilla")
 
 #shebang is added to specify the interpreter that should be used to run the program
